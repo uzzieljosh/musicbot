3404804a777c5496b4c8da719f7c81c5513ef4d9
Uzziel Josh Abiday <urabiday@up.edu.ph>
2023-04-14 20:36:46 +0800

    redeploy test

--- commands/ping.js
+++ commands/ping.js
@@ -2,6 +2,6 @@ module.exports ={
     name: 'hello',
     description: "uzziel music bot",
     execute(client,message,args,cmd,Discord){
-        message.channel.send('bobo ka! ./.');
+        message.channel.send('Bobo ka! ./.');
     }
 }
